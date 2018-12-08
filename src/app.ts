950203ad9c293a22a1b4be704e9efdf6e154c24c
Zoltan Csenyi <cszoltan422@gmail.com>
2018-12-08 23:21:38 +0100

    Set mongo db name env variable to default

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 const host = process.env.MONGODB_HOST;
 const mongoPort = process.env.MONGODB_PORT;
-const db = process.env.MONGODB_DBNAME_PROD;
+const db = process.env.MONGODB_DBNAME;
 const port = "8080";
 
 mongoose.connect(`mongodb://${username}:${password}@${host}:${mongoPort}/${db}`).then(() => {
