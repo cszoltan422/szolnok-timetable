b909d5ec2175d20c5e620d174959b2f2d5ade77b
Zoltan Csenyi <cszoltan422@gmail.com>
2018-12-08 11:53:09 +0100

    Add deploy scripts

---
+++ deploy/build-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ~/.profile
+echo "Used constants:"
+echo $SZOLNOK_APP_HOME
+echo $SERVER_USERNAME
+echo $SERVER_1
+echo $SERVER_2
+
+echo "Building docker image"
+cd ${SZOLNOK_APP_HOME}/szolnok-timetable-backend
+sudo docker build --tag szolnok-timetable .
+imageid="$(sudo docker images -q szolnok-timetable)"
+echo "IMAGE_ID: ${imageid}"
+commitHash="$(git rev-parse HEAD | cut -c 1-8)"
+timestamp="$(date +%x-%H-%M-%S)"
+dockerTag=${timestamp}-${commitHash}
+echo $dockerTag
+sudo docker tag ${imageid} cszoltan422/szolnok-timetable:latest
+sudo docker tag ${imageid} cszoltan422/szolnok-timetable:${dockerTag}
+sudo docker push cszoltan422/szolnok-timetable
+sudo docker push cszoltan422/szolnok-timetable:${dockerTag}
