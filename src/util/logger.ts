c745d75d5089cc0e75e8ca75963b2a087a0588e2
Zoltan Csenyi <cszoltan422@gmail.com>
2019-02-04 22:52:07 +0100

    Add moment to dependencies

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,4 +1,7 @@
 import winston from "winston";
+import moment from "moment";
+
+const timestampFormat = () => moment().format("YYYY-MM-DD hh:mm:ss").trim();
 
 const logformat = winston.format.printf((info: any) => {
     const logEntry = {
@@ -13,7 +16,7 @@ const logger = winston.createLogger({
     level: "info",
     format: winston.format.combine(
         winston.format.timestamp({
-            format: "YYYY-MM-DD hh:mm:ss"
+            format: timestampFormat()
         }),
         logformat
     ),
