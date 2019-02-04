be6ed5f3f4ca4e3f2d0de671b7d71e3fe9126b26
Zoltan Csenyi <cszoltan422@gmail.com>
2019-02-04 22:15:43 +0100

    Fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,14 @@ import busesRouter from "./controller/bus.controller";
 import busStopsRouter from "./controller/busstops.controller";
 import timetableRouter from "./controller/timetable.controller";
 
-const database_url = process.env.MONGODB_URL;
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const host = process.env.MONGODB_HOST;
+const mongoPort = process.env.MONGODB_PORT;
+const db = process.env.MONGODB_DBNAME;
+const port = "8080";
 
-mongoose.connect(database_url).then(() => {
+mongoose.connect(`mongodb://${username}:${password}@${host}:${mongoPort}/${db}`).then(() => {
     logger.info("Successfully connected to database!");
   }, (err: mongoose.Error) => {
       logger.error(JSON.stringify(err));
