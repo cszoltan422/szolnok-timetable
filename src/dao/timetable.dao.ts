0e5ae94b025729e68b2f3e2271bc1c80596de2d5
Zoltan Csenyi <cszoltan422@gmail.com>
2019-02-10 19:47:27 +0100

    Add occurrence to log in timetable dao

--- src/dao/timetable.dao.ts
+++ src/dao/timetable.dao.ts
@@ -5,7 +5,7 @@ import getTimetable from "./../transformer/timetable.transformer";
 
 export default function getTimeTableOfBusStop(busName: string, startStop: string, busStop: string, occurrence: string): Promise<TimetableResponse> {
     return new Promise((resolve, reject) => {
-        logger.info(`Fetching timetable of [${busName}] from [${startStop}] in busStop [${busStop}]`);
+        logger.info(`Fetching timetable of [${busName}] from [${startStop}] in busStop [${busStop}] with occurrence [${occurrence}]`);
         Bus.findOne({ "busName": busName }, (err: any, data: any) => {
             getTimetable(err, data, busName, busStop, startStop, occurrence)
             .then((timetable) => {
